ec9689316977ec841e84d7076db4aeeb0a87928d
Aadil Mugal <aadilshe786@gmail.com>
2023-03-05 22:23:10 +0530

    optimised code for footer and navbar

--- src/data/footer/index.js
+++ src/data/footer/index.js
@@ -1,3 +1,7 @@
+import React from "react"
+import { HeartIcon } from "@heroicons/react/24/solid"
+import { FiArrowUp } from "react-icons/fi"
+
 const data = {
   copyrightChar: "© ",
   year: `${new Date().getFullYear()} `,
@@ -8,12 +12,20 @@ const data = {
 
   made: {
     text1: "Made with ",
+    heartIcon: <HeartIcon className="inline-block h-4 w-4 text-red-400 " />,
     text2: " and ",
     gatsby: {
       label: "gatsby",
       link: "https://www.gatsbyjs.com",
     },
   },
+
+  goToTopIcon: (
+    <FiArrowUp
+      size={19}
+      className="text-slate-500 hover:text-slate-900 dark:text-slate-400 dark:hover:text-sky-300"
+    />
+  ),
 }
 
 export default data
