be59b6ab53cd8a0c243360bc8fd9c11013cdcb97
Aadil Mugal <aadilshe786@gmail.com>
2023-03-05 15:07:26 +0530

    add footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,41 @@
 import React from "react"
+import { HeartIcon } from "@heroicons/react/24/solid"
+import { FaArrowUp } from "react-icons/fa"
+import data from "../../data/footer/index"
+
+const Anchor = ({ label, link }) => (
+  <a href={link} className="hover:text-sky-900 dark:hover:text-sky-300">
+    {label}
+  </a>
+)
 
 const Footer = () => {
-  return <footer>© {new Date().getFullYear()} Aadil Mugal</footer>
+  return (
+    <div className="supports-backdrop-blur:bg-white/60 z-40 flex items-center border-t  border-slate-900/10 bg-white/95 px-4  text-slate-600 backdrop-blur dark:border-slate-50/[0.06] dark:bg-transparent dark:text-slate-300 sm:px-8">
+      <div className="flex w-full items-center border-r border-slate-900/10 py-3 pr-8 dark:border-slate-50/[0.1] sm:justify-center sm:gap-4">
+        <div>
+          {`${data.copyrightChar} ${data.year}`}
+          <Anchor {...data.dev} />
+        </div>
+        <div className="hidden border-l border-slate-900/10 pl-4 dark:border-slate-50/[0.1] sm:block">
+          {data.made.text1}
+          <HeartIcon className="inline-block h-4 w-4 text-red-400 " />
+          {data.made.text2}
+          <Anchor {...data.made.gatsby} />
+        </div>
+      </div>
+
+      {/* go to top btn */}
+      <div className="ml-4 flex h-full items-center justify-center sm:ml-8 ">
+        <a href="#">
+          <FaArrowUp
+            size={19}
+            className="text-slate-500 hover:text-slate-900 dark:text-slate-400 dark:hover:text-sky-300"
+          />
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
---
+++ src/data/footer/index.js
@@ -0,0 +1,19 @@
+const data = {
+  copyrightChar: "© ",
+  year: `${new Date().getFullYear()} `,
+  dev: {
+    label: "Aadil Mugal",
+    link: "https://github.com/aadilmughal786",
+  },
+
+  made: {
+    text1: "Made with ",
+    text2: " and ",
+    gatsby: {
+      label: "gatsby",
+      link: "https://www.gatsbyjs.com",
+    },
+  },
+}
+
+export default data
