c6b97c65f154dab1a5d3faad92dba92a92bdfe3c
Aadil Mugal <aadilshe786@gmail.com>
2023-03-07 02:04:29 +0530

    create skill section

---
+++ src/data/home/skill-section.js
@@ -0,0 +1,48 @@
+const data = {
+  label: "Skills",
+
+  skills: [
+    {
+      id: 1,
+      label: "HTML",
+      per: 95,
+    },
+    {
+      id: 2,
+      label: "CSS",
+      per: 80,
+    },
+    {
+      id: 3,
+      label: "JavaScript",
+      per: 85,
+    },
+    {
+      id: 4,
+      label: "Typescript",
+      per: 70,
+    },
+    {
+      id: 5,
+      label: "ReactJS",
+      per: 80,
+    },
+    {
+      id: 6,
+      label: "NodeJS",
+      per: 85,
+    },
+    {
+      id: 7,
+      label: "ExpressJS",
+      per: 75,
+    },
+    { id: 8, label: "MongoDb", per: 70 },
+    { id: 9, label: "Python", per: 90 },
+    { id: 10, label: "Figma", per: 80 },
+    { id: 11, label: "Git/GitHub", per: 60 },
+    { id: 12, label: "Linux", per: 88 },
+  ],
+}
+
+export default data
