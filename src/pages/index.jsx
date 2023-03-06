8cc7c134fd80a7b431bb4812e028d74f2f12e060
Aadil Mugal <aadilshe786@gmail.com>
2023-03-06 18:49:36 +0530

    add component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,14 @@ import * as React from "react"
 import Seo from "../components/seo/seo"
 import Layout from "../components/layouts/layout"
 import IntroSection from "../components/intro-section/intro-section"
+import SkillSection from "../components/skill-section/skill-section"
 
 const HomePage = () => (
   <Layout>
+    {/* <IntroSection />
     <IntroSection />
-    <IntroSection />
-    <IntroSection />
+    <IntroSection /> */}
+    <SkillSection />
   </Layout>
 )
 
