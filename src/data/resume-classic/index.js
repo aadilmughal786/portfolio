33ca7c15960dad538c48f256fbea6168175e2d30
Aadil Mugal <aadilshe786@gmail.com>
2023-03-08 00:42:16 +0530

    add follow section

--- src/data/resume-classic/index.js
+++ src/data/resume-classic/index.js
@@ -2,12 +2,14 @@ import eduData from "./education"
 import contactData from "./contact"
 import courseData from "./course"
 import skillData from "./skill"
+import followData from "./follow"
 
 const data = {
   eduData,
   contactData,
   courseData,
   skillData,
+  followData,
 }
 
 export default data
--- src/pages/resume-classic.jsx
+++ src/pages/resume-classic.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layouts/layout"
 import Card from "../components/resume-classic/card"
 import CardItem from "../components/resume-classic/card-item"
@@ -10,11 +10,15 @@ import Seo from "../components/seo/seo"
 import data from "../data/resume-classic/index"
 
 const Resume = () => {
+  // state for follow section
+  const [isExpand, setIsExpand] = useState(false)
+
   return (
     <Layout>
-      <div className="grid gap-x-4 gap-y-8 px-4 py-16 sm:px-8 lg:grid-cols-3">
-        {/* Education */}
-        <div className="lg:col-span-2">
+      <div className="grid gap-x-4 px-4 py-16 sm:px-8 lg:grid-cols-3">
+        {/* column-1 */}
+        <div className="flex flex-col gap-y-8 lg:col-span-2">
+          {/* Education */}
           <Card icon={data.eduData.icon} title={data.eduData.title}>
             {data.eduData.item.map(item => (
               <CardItem key={item.id}>
@@ -22,10 +26,20 @@ const Resume = () => {
               </CardItem>
             ))}
           </Card>
+
+          {/* Courses & Certificates */}
+          <Card icon={data.courseData.icon} title={data.courseData.title}>
+            {data.courseData.item.map(item => (
+              <CardItem key={item.id}>
+                <Course data={item} />
+              </CardItem>
+            ))}
+          </Card>
         </div>
 
-        {/* Contact */}
-        <div>
+        {/* column-2 */}
+        <div className="flex flex-col gap-y-8">
+          {/* Contact */}
           <Card icon={data.contactData.icon} title={data.contactData.title}>
             {data.contactData.item.map(item => (
               <CardItem key={item.id}>
@@ -41,22 +55,8 @@ const Resume = () => {
               </div>
             </CardItem>
           </Card>
-        </div>
 
-        {/* Courses & Certificates */}
-        <div className="lg:col-span-2">
-          <Card icon={data.courseData.icon} title={data.courseData.title}>
-            {data.courseData.item.map(item => (
-              <CardItem key={item.id}>
-                <Course data={item} />
-              </CardItem>
-            ))}
-          </Card>
-        </div>
-
-        {/* Skills */}
-
-        <div>
+          {/* Skills */}
           <Card icon={data.skillData.icon} title={data.skillData.title}>
             {data.skillData.items.map(item => (
               <CardItem key={item.id}>
@@ -66,6 +66,42 @@ const Resume = () => {
                 ))}
               </CardItem>
             ))}
+          </Card>
+
+          {/* follow */}
+          <Card icon={data.followData.icon} title={data.followData.title}>
+            {data.followData.items.slice(0, 3).map(item => (
+              <CardItem key={item.id}>
+                <Contact data={item} />
+              </CardItem>
+            ))}
+            {/* hidden */}
+            <div className={isExpand ? "block" : "hidden"}>
+              {data.followData.items
+                .slice(3, data.followData.items.length)
+                .map(item => (
+                  <CardItem key={item.id}>
+                    <Contact data={item} />
+                  </CardItem>
+                ))}
+            </div>
+            <div className="cursor-pointer  py-1">
+              {isExpand ? (
+                <div
+                  onClick={e => setIsExpand(false)}
+                  className="flex justify-center"
+                >
+                  {data.followData.arrowUp}
+                </div>
+              ) : (
+                <div
+                  onClick={e => setIsExpand(true)}
+                  className="flex justify-center"
+                >
+                  {data.followData.arrowDown}
+                </div>
+              )}
+            </div>
           </Card>
         </div>
       </div>
