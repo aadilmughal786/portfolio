5ed1ff576361115b8b69c71a62be6dc51e5d94dd
Aadil Mugal <64457787+aadilmughal786@users.noreply.github.com>
2023-03-08 22:13:28 +0530

    update card borders (#12)

--- src/components/resume-classic/card-item.jsx
+++ src/components/resume-classic/card-item.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const CardItem = ({ children }) => {
   return (
-    <div className="border-b border-slate-900/10 px-6 py-4 dark:border-slate-50/[0.06]">
+    <div className="border-b border-slate-900/10 px-6 py-4 dark:border-slate-50/[0.10]">
       {children}
     </div>
   )
