1d561cfb7c1b7872d4ff75095397146f00f2d2e9
Aadil Mugal <aadilshe786@gmail.com>
2023-03-14 02:08:36 +0530

    add font

--- src/components/home/intro-section/intro-section.jsx
+++ src/components/home/intro-section/intro-section.jsx
@@ -8,11 +8,11 @@ import Typewriter from "typewriter-effect"
 
 const IntroSection = () => {
   return (
-    <div className="flex items-center bg-sky-300/10 py-8 dark:bg-transparent md:py-16">
+    <div className="flex items-center py-16">
       <div className="flex flex-col-reverse justify-center gap-6 px-4 text-slate-700 dark:text-slate-200 sm:px-8 md:flex-row md:items-center md:gap-12">
         <div className="flex basis-1/2 flex-col items-center gap-6 text-center md:items-start md:pr-4 md:text-start lg:basis-2/5">
           <div className="waving-hand w-min text-4xl">👋</div>
-          <div className="font-[Courgette] text-5xl">I'M Aadil</div>
+          <div className="font-[Kurale] text-6xl">I'M Aadil</div>
           <div className="flex flex-wrap items-center gap-1 font-mono text-xl">
             I
             <Typewriter
@@ -23,7 +23,7 @@ const IntroSection = () => {
               }}
             />
           </div>
-          <div>
+          <div className="font-[Kurale] text-xl text-sky-700 dark:text-sky-300">
             I am a self-taught front-end developer based in Rajasthan, India. I
             can develop responsive websites from scratch and raise them into
             modern user-friendly web experiences
