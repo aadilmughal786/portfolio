27d7748048d6459ca07633e231d14bc01b8f84fc
Aadil Mugal <aadilshe786@gmail.com>
2023-03-15 22:23:04 +0530

    add projects

---
+++ src/data/projects/projects.js
@@ -0,0 +1,106 @@
+import React from "react"
+import { SiGithub } from "react-icons/si"
+import { HiExternalLink } from "react-icons/hi"
+import sample from "../../images/porojects/projects/sample.png"
+const data = {
+  label: "Projects",
+  projects: [
+    {
+      id: 1,
+      img: sample,
+      chip: "new",
+      links: [
+        {
+          icon: <SiGithub size={20} />,
+          href: "https://github.com/aadilmughal786/simple-web-project/blob/main/acal.html",
+        },
+        {
+          icon: <HiExternalLink size={25} />,
+          href: "https://aadilmughal786.github.io/simple-web-project/acal.html",
+        },
+      ],
+      name: "ACulator",
+      description:
+        "It is a simple calculator which do basic stuff computation and fully responsive in any device",
+      tools: ["JavaScript", "html", "CSS"],
+    },
+    {
+      id: 2,
+      img: sample,
+      chip: "new",
+      links: [
+        {
+          icon: <SiGithub size={20} />,
+          href: "https://github.com/aadilmughal786/simple-web-project/blob/main/acal.html",
+        },
+        {
+          icon: <HiExternalLink size={25} />,
+          href: "https://aadilmughal786.github.io/simple-web-project/acal.html",
+        },
+      ],
+      name: "ACulator",
+      description:
+        "It is a simple calculator which do basic stuff computation and fully responsive in any device",
+      tools: ["JavaScript", "html", "CSS"],
+    },
+    {
+      id: 3,
+      img: sample,
+      chip: "new",
+      links: [
+        {
+          icon: <SiGithub size={20} />,
+          href: "https://github.com/aadilmughal786/simple-web-project/blob/main/acal.html",
+        },
+        {
+          icon: <HiExternalLink size={25} />,
+          href: "https://aadilmughal786.github.io/simple-web-project/acal.html",
+        },
+      ],
+      name: "ACulator",
+      description:
+        "It is a simple calculator which do basic stuff computation and fully responsive in any device",
+      tools: ["JavaScript", "html", "CSS"],
+    },
+    {
+      id: 4,
+      img: sample,
+      chip: "new",
+      links: [
+        {
+          icon: <SiGithub size={20} />,
+          href: "https://github.com/aadilmughal786/simple-web-project/blob/main/acal.html",
+        },
+        {
+          icon: <HiExternalLink size={25} />,
+          href: "https://aadilmughal786.github.io/simple-web-project/acal.html",
+        },
+      ],
+      name: "ACulator",
+      description:
+        "It is a simple calculator which do basic stuff computation and fully responsive in any device",
+      tools: ["JavaScript", "html", "CSS"],
+    },
+    {
+      id: 5,
+      img: sample,
+      chip: "new",
+      links: [
+        {
+          icon: <SiGithub size={20} />,
+          href: "https://github.com/aadilmughal786/simple-web-project/blob/main/acal.html",
+        },
+        {
+          icon: <HiExternalLink size={25} />,
+          href: "https://aadilmughal786.github.io/simple-web-project/acal.html",
+        },
+      ],
+      name: "ACulator",
+      description:
+        "It is a simple calculator which do basic stuff computation and fully responsive in any device",
+      tools: ["JavaScript", "html", "CSS"],
+    },
+  ],
+}
+
+export default data
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -4,13 +4,15 @@ import Seo from "../components/seo/seo"
 import data from "../data/projects/index"
 import HeroSection from "../components/projects/hero-section/hero-section"
 import Toy from "../components/projects/toy/toy"
+import Project from "../components/projects/projects/projects"
 
-const Contact = () => {
+const Projects = () => {
   return (
     <div>
       <Layout>
         <HeroSection data={data.heroData} />
         {/* Toy Projects */}
+        <Project data={data.projectData} />
         <Toy data={data.toyData} />
       </Layout>
     </div>
@@ -19,4 +21,4 @@ const Contact = () => {
 
 export const Head = () => <Seo title="Contact" />
 
-export default Contact
+export default Projects
