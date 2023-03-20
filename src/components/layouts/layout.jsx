43cb075f9264c099b3b7f56dd06eb7d0b42b2790
Aadil Mugal <aadilshe786@gmail.com>
2023-03-21 00:26:16 +0530

    added quick tools which are native to this app

--- src/components/layouts/layout.jsx
+++ src/components/layouts/layout.jsx
@@ -4,7 +4,7 @@ import NavBar from "../nav-bar/nav-bar"
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex min-h-screen flex-col justify-between text-slate-600 transition-colors dark:bg-slate-800 dark:text-slate-300">
+    <div className="flex min-h-screen flex-col justify-between text-slate-600 transition-colors duration-150 ease-in dark:bg-slate-800 dark:text-slate-300">
       <NavBar />
       <div className="flex-1">
         <main className="overflow-hidden">{children}</main>
