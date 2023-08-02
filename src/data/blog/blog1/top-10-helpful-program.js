ff72aa55023f8237d42c149405cb2fa57cf04bdc
Aadil Mugal <64457787+aadilmughal786@users.noreply.github.com>
2023-08-02 23:08:52 +0530

    update: Update top-10-helpful-program.js

--- src/data/blog/blog1/top-10-helpful-program.js
+++ src/data/blog/blog1/top-10-helpful-program.js
@@ -9,7 +9,7 @@ export const metaData = {
   chip: "New",
   route: "/blog/top-10-helpful-program",
   description: `Here are 10 most helpful program code snippets that I use during coding contest which can be used as subroutine`,
-  tags: ["C++"],
+  tags: ["C++", "Competitive Programming", "Problem Solving"],
 }
 
 // Labels => p, h2, img, code, note
