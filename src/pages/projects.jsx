90db1b797b2c4bffd9bec0422e4cb9582d8af11d
Aadil Mugal <aadilshe786@gmail.com>
2024-01-02 20:57:56 +0530

    rm : remove code section

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -5,8 +5,6 @@ import data from "../data/projects/index"
 import HeroSection from "../components/projects/hero-section/hero-section"
 import Toy from "../components/projects/toy/toy"
 import Project from "../components/projects/projects/projects"
-import SecondaryBtn from "../components/buttons/secondary-btn"
-import { ArrowLongRightIcon } from "@heroicons/react/24/solid"
 
 const Projects = () => {
   return (
@@ -14,20 +12,6 @@ const Projects = () => {
       <Layout>
         <HeroSection data={data.heroData} />
         <Project data={data.projectData} />
-
-        <div className="px-4 py-16 sm:px-8 lg:px-16">
-          {/* Code snippet library */}
-          <div className="flex flex-col items-center justify-between gap-y-6 rounded border border-dashed bg-sky-300/10 p-4 tracking-wider text-sky-700 shadow-sky-800 transition-all duration-200 ease-in hover:shadow-md dark:border-slate-800  dark:bg-slate-700 dark:text-sky-300 dark:shadow-sky-300 sm:flex-row">
-            <span className="text-xl font-semibold">
-              Code Snippet library in C++
-            </span>
-            <SecondaryBtn
-              label={"View"}
-              icon={<ArrowLongRightIcon />}
-              to="/code"
-            />
-          </div>
-        </div>
 
         {/* Toy Projects */}
         <Toy data={data.toyData} />
